5396af6db747605d5cf09e2b517d745cd3aed9ee
Alexandre Emion <aemion@umanit.fr>
2024-12-02 13:40:39 +0100

    refactor: input name

--- src/AbstractSolver.php
+++ src/AbstractSolver.php
@@ -14,6 +14,21 @@ abstract class AbstractSolver
         return false;
     }
 
+    public function getDay(): string
+    {
+        $fqcn = self::class;
+        $splitFqcn = explode('\\', $fqcn);
+
+        $className = array_pop($splitFqcn);
+
+        $day = str_replace('Day', '', $className);
+        if (\strlen($day) === 1) {
+            $day = '0' . $day;
+        }
+
+        return $day;
+    }
+
     abstract public function loadInput(string $path): void;
 
     public function preSolve(): void
